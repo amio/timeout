de73f6dc6f185cd05ca32ee1fa60fe13ab30239e
amio <amio.cn@gmail.com>
2021-07-12 17:44:52 +0800

    chore: add ga count

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const http = require('http')
+const { measure } = require('measurement-protocol')
 
 module.exports = async function (req, res) {
   const { accept } = req.headers
@@ -8,6 +9,8 @@ module.exports = async function (req, res) {
     const readmeFile = path.join(__dirname, '../README.md')
     return res.end(fs.readFileSync(readmeFile))
   }
+
+  measure('UA-4646421-13').pageview(req.url).send()
 
   const ms = Math.min(Number(req.url.replace('/', '')) || 0, 10000)
   await new Promise(resolve => setTimeout(resolve, ms))
